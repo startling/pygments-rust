4d3e97a90a95b01821fbe771338e0d85422c5818
tim <tdixon51793@gmail.com>
2012-01-20 16:09:57 -0600

    Base `setup.py`, `.gitignore`, and `__init__.py`.
    Wooh.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import pygments_rust
+
+
+setup(
+    name = "pygments_rust",
+    version = pygments_rust.__version__,
+    author = "startling",
+    author_email = "tdixon51793@gmail.com",
+    description = "Rust syntax highlighting with pygments!",
+    packages = ["pygments_rust"],
+    install_requires = ['pygments'],
+)
