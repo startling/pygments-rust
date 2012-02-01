1a243233ba50eee0e81ee51088241508a5279638
Ian D. Bollinger <ian.bollinger@gmail.com>
2012-02-01 02:29:26 -0500

    Added highlighting for function and type names, and for attributes.

--- pygments_rust/lexer.py
+++ pygments_rust/lexer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pygments.lexer import RegexLexer, include
-from pygments.token import *
+from pygments.lexer import RegexLexer, bygroups, combined, include
+from pygments.token import Error, Text, Other, Comment, Operator, Keyword, \
+    Name, String, Number, Generic, Punctuation, Whitespace
 from pygments.unistring import xid_start, xid_continue
 
 
@@ -12,26 +13,44 @@ class RustLexer(RegexLexer):
     aliases = ['rust']
     filenames = ['*.rc', '*.rs']
 
+    _identifier = r'[%s][%s]*' % (xid_start, xid_continue)
+    _path = '(?:' + _identifier + r'::)*' + _identifier + '(?:::)?'
+
     tokens = {
         'root': [
             # Literals
             include('literals'),
             # Comments
             include('comments'),
-            # Operators
-            include('operators'),
-            # Keywords
-            include('keywords'),
             # Types
             include('types'),
-            # Symbols -- braces, parentheses, semicolons, etc.
-            # http://doc.rust-lang.org/doc/rust.html#symbols
-            (r'([](){};#,:.[]|::|-\>)', Punctuation),
-            # Identifiers
-            # http://doc.rust-lang.org/doc/rust.html#identifiers
-            (r'[%s][%s]*' % (xid_start, xid_continue), Name),
             # Whitespace is insignificant.
             (r'[ \t\n\r]+', Whitespace),
+            (r'(fn)([ \t\n\r]+)', bygroups(Keyword, Whitespace), 'funcname'),
+            (r'(const)([ \t\n\r]+)', bygroups(Keyword, Whitespace), 'constname'),
+            (r'(enum|iface|type)([ \t\n\r]+)', bygroups(Keyword, Whitespace), 'typename'),
+            (r'(impl)([ \t\n\r]+)', bygroups(Keyword, Whitespace), 'impl'),
+            (r'(import|use)([ \t\n\r]+)', bygroups(Keyword, Whitespace), 'import'),
+            # Keywords
+            include('keywords'),
+            # Identifiers
+            # http://doc.rust-lang.org/doc/rust.html#identifiers
+            (r'#' + _identifier, Comment.Preproc),
+            (r'([](){};,:.[]|::|-\>)', Punctuation),
+            # Operators
+            include('operators'),
+            (r'#\[', Name.Attribute, combined('attributes', 'root')),
+            #(r'#\[(.|\n)*?\]', Name.Attribute),
+            # Symbols -- braces, parentheses, semicolons, etc.
+            # http://doc.rust-lang.org/doc/rust.html#symbols
+            (r'#', Punctuation),
+            (_identifier, Name),
+        ],
+        'attributes': [
+            (r'\[', Name.Attribute, '#push'),
+            (r'\];?', Name.Attribute, '#pop'),
+            (r'[(=,)]', Name.Attribute),
+            (_identifier, Name.Attribute),
         ],
         'literals': [
             # http://doc.rust-lang.org/doc/rust.html#literals
@@ -50,6 +69,24 @@ class RustLexer(RegexLexer):
             (r'\d(\d|_)*(\.(\d|_)+)?((e|E)[+-]?(\d|_)+)?(f|f32|f64)', Number.Float),
             # Decimal integer literals
             (r'\d(\d|_)*(i8|i16|i32|i64|u|u8|u16|u32|u64)?', Number.Integer),
+        ],
+        'funcname': [
+            (_identifier, Name.Function, '#pop')
+        ],
+        'constname': [
+            (_identifier, Name.Constant, '#pop')
+        ],
+        'typename': [
+            (_identifier, Name.Class, '#pop')
+        ],
+        'impl': [
+            (_identifier, Name.Class, '#pop'),
+            ('<', Operator, '#pop')
+        ],
+        'import': [
+            (r'(' + _identifier + r')([ \t\n\r]*)(=)([ \t\n\r]*)(' + _path + r')',
+             bygroups(Name, Whitespace, Punctuation, Whitespace, Name.Namespace), '#pop'),
+            (_path, Name.Namespace, '#pop')
         ],
         'string': [
             (r'\\(\n|[nrt"\\]|x[0-9a-fA-F_]{2}|u[0-9a-fA-F_]{4}|U[0-9a-fA-F_]{8})', String.Escape),
@@ -85,20 +122,19 @@ class RustLexer(RegexLexer):
         ],
         'comments': [
             # Single-line comments; e.g. //this is a comment
-            (r'//.*', Comment),
+            (r'//.*', Comment.Single),
             # Multi-line comments; e.g., /* this is a \n comment */
-            (r'/\*(.|\n)*?\*/', Comment)
+            (r'/\*(.|\n)*?\*/', Comment.Multiline)
         ],
         'keywords': [
             # General keywords
-            (r'(alt|as|assert|be|bind|break|chan|check|claim|cont|const|'
-             r'copy|do|else|enum|export|fail|fn|for|if|iface|impl|import|in|'
+            (r'(alt|as|assert|be|bind|break|chan|check|claim|cont|'
+             r'copy|do|else|export|fail|fn|for|if|in|'
              r'inline|let|log|mod|move|mutable|native|of|pure|resource|ret|'
-             r'self|send|type|unchecked|unsafe|use|while|with)\b', Keyword),
+             r'self|send|to|unchecked|unsafe|while|with)\b', Keyword),
+            (r'(class|trait)\b', Keyword.Reserved),
             # Unimplemented keywords
-            (r'(note|prove)\b', Keyword),
-            # Reserved keywords
-            (r'(class|trait)\b', Keyword),
+            (r'(note|prove)\b', Keyword.Reserved),
             # Booleans
             (r'(true|false)\b', Keyword.Constant),
             # Nil
@@ -107,10 +143,10 @@ class RustLexer(RegexLexer):
         'types': [
             # Types
             (r'(int|uint|float|char|bool|u8|u16|u32|u64|f32|f64|i8|i16|i32|'
-             r'i64|str|task|vec)\b', Name.Builtin),
+             r'i64|str|task|vec)\b', Keyword.Type),
             # Unimplemented types
-            (r'any\b', Name.Builtin),
+            (r'any\b', Keyword.Reserved),
             # Reserved types
-            (r'(m32|m64|m128|f80|f16|f128)\b', Name.Builtin),
+            (r'(m32|m64|m128|f80|f16|f128)\b', Keyword.Reserved),
         ]
     }
